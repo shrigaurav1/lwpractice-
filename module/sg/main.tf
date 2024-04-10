c4a48414e38309605e7e2b6fcbf7d41d7c68d515
Gaurav <94680592+shrigaurav1@users.noreply.github.com>
2024-04-11 04:09:06 +0530

    Update main.tf

--- module/sg/main.tf
+++ module/sg/main.tf
@@ -6,14 +6,14 @@ resource "aws_security_group" "gaurav_sg" {
  }
 }
 resource "aws_vpc_security_group_ingress_rule" "ib" {
- security_group_id = aws_security_group.vikas_sg.id
- cidr_ipv4 = "${aws_eip.vikaseip.public_ip}/32"
+ security_group_id = aws_security_group.gaurav_sg.id
+ cidr_ipv4 = "${aws_eip.gauravip.public_ip}/32"
  from_port = 443
  ip_protocol = "tcp"
  to_port = 443
 }
 resource "aws_vpc_security_group_egress_rule" "ob" {
  security_group_id = aws_security_group.gaurav_sg.id
- cidr_ipv4 = "${aws_eip.vikaseip.public_ip}/32"
+ cidr_ipv4 = "${aws_eip.gauravip.public_ip}/32"
  ip_protocol = "-1" # semantically equivalent to all ports
 }
